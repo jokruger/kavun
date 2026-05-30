7ea5372d58289e4057b5cfe935fe3ee9a33908da
Yurii Biurher <yurii.biurher@zorallabs.com>
2026-05-30 10:42:15 +0300

    value hooks to always accept arena

--- core/value.go
+++ core/value.go
@@ -30,18 +30,18 @@ func (v *Value) Set(val Value) {
 	*v = val
 }
 
-func (v Value) EncodeJSON() ([]byte, error) {
-	b, err := ValueTypes[v.Type].EncodeJSON(v)
+func (v Value) EncodeJSON(a *Arena) ([]byte, error) {
+	b, err := ValueTypes[v.Type].EncodeJSON(a, v)
 	if err != nil {
-		return nil, fmt.Errorf("json encoding failed for type %s: %w", v.TypeName(), err)
+		return nil, fmt.Errorf("json encoding failed for type %s: %w", v.TypeName(a), err)
 	}
 	return b, nil
 }
 
-func (v Value) EncodeBinary() ([]byte, error) {
-	b, err := ValueTypes[v.Type].EncodeBinary(v)
+func (v Value) EncodeBinary(a *Arena) ([]byte, error) {
+	b, err := ValueTypes[v.Type].EncodeBinary(a, v)
 	if err != nil {
-		return nil, fmt.Errorf("binary encoding failed for type %s: %w", v.TypeName(), err)
+		return nil, fmt.Errorf("binary encoding failed for type %s: %w", v.TypeName(a), err)
 	}
 	i := byte(0)
 	if v.Immutable {
@@ -50,11 +50,7 @@ func (v Value) EncodeBinary() ([]byte, error) {
 	return append([]byte{v.Type, i}, b...), nil
 }
 
-func (v Value) GobEncode() ([]byte, error) {
-	return v.EncodeBinary()
-}
-
-func (v *Value) DecodeBinary(data []byte) error {
+func (v *Value) DecodeBinary(a *Arena, data []byte) error {
 	if len(data) < 2 {
 		return fmt.Errorf("binary decoding failed (type header): expected at least 2 bytes for type, got %d", len(data))
 	}
@@ -62,7 +58,7 @@ func (v *Value) DecodeBinary(data []byte) error {
 	var t Value
 	t.Type = data[0]
 	t.Immutable = data[1] != 0
-	if err := ValueTypes[t.Type].DecodeBinary(&t, data[2:]); err != nil {
+	if err := ValueTypes[t.Type].DecodeBinary(a, &t, data[2:]); err != nil {
 		return fmt.Errorf("binary decoding failed for type %d: %w", t.Type, err)
 	}
 	*v = t
@@ -70,172 +66,168 @@ func (v *Value) DecodeBinary(data []byte) error {
 	return nil
 }
 
-func (v *Value) GobDecode(data []byte) error {
-	return v.DecodeBinary(data)
-}
-
-func (v Value) Next() bool {
-	return ValueTypes[v.Type].Next(v)
+func (v Value) Next(a *Arena) bool {
+	return ValueTypes[v.Type].Next(a, v)
 }
 
 func (v Value) Key(a *Arena) (Value, error) {
-	return ValueTypes[v.Type].Key(v, a)
+	return ValueTypes[v.Type].Key(a, v)
 }
 
 func (v Value) Value(a *Arena) (Value, error) {
-	return ValueTypes[v.Type].Value(v, a)
+	return ValueTypes[v.Type].Value(a, v)
 }
 
-func (v Value) TypeName() string {
-	return ValueTypes[v.Type].Name(v)
+func (v Value) TypeName(a *Arena) string {
+	return ValueTypes[v.Type].Name(a, v)
 }
 
-func (v Value) Format(sp fspec.FormatSpec) (string, error) {
-	return ValueTypes[v.Type].Format(v, sp)
+func (v Value) Format(a *Arena, sp fspec.FormatSpec) (string, error) {
+	return ValueTypes[v.Type].Format(a, v, sp)
 }
 
-func (v Value) String() string {
-	return ValueTypes[v.Type].String(v)
+func (v Value) String(a *Arena) string {
+	return ValueTypes[v.Type].String(a, v)
 }
 
-func (v Value) Interface() any {
-	return ValueTypes[v.Type].Interface(v)
+func (v Value) Interface(a *Arena) any {
+	return ValueTypes[v.Type].Interface(a, v)
 }
 
-func (v Value) Arity() int8 {
-	return ValueTypes[v.Type].Arity(v)
+func (v Value) Arity(a *Arena) int8 {
+	return ValueTypes[v.Type].Arity(a, v)
 }
 
 func (v Value) IsUserDefined() bool {
 	return v.Type >= VT_USER_DEFINED
 }
 
-func (v Value) IsTrue() bool {
-	return ValueTypes[v.Type].IsTrue(v)
+func (v Value) IsTrue(a *Arena) bool {
+	return ValueTypes[v.Type].IsTrue(a, v)
 }
 
-func (v Value) IsIterable() bool {
-	return ValueTypes[v.Type].IsIterable(v)
+func (v Value) IsIterable(a *Arena) bool {
+	return ValueTypes[v.Type].IsIterable(a, v)
 }
 
-func (v Value) IsCallable() bool {
-	return ValueTypes[v.Type].IsCallable(v)
+func (v Value) IsCallable(a *Arena) bool {
+	return ValueTypes[v.Type].IsCallable(a, v)
 }
 
-func (v Value) IsVariadic() bool {
-	return ValueTypes[v.Type].IsVariadic(v)
+func (v Value) IsVariadic(a *Arena) bool {
+	return ValueTypes[v.Type].IsVariadic(a, v)
 }
 
-func (v Value) Contains(e Value) bool {
-	return ValueTypes[v.Type].Contains(v, e)
+func (v Value) Contains(a *Arena, e Value) bool {
+	return ValueTypes[v.Type].Contains(a, v, e)
 }
 
 func (v Value) AsValue() (Value, bool) {
 	return v, true
 }
 
-func (v Value) AsBool() (bool, bool) {
-	return ValueTypes[v.Type].AsBool(v)
+func (v Value) AsBool(a *Arena) (bool, bool) {
+	return ValueTypes[v.Type].AsBool(a, v)
 }
 
-func (v Value) AsRune() (rune, bool) {
-	return ValueTypes[v.Type].AsRune(v)
+func (v Value) AsRune(a *Arena) (rune, bool) {
+	return ValueTypes[v.Type].AsRune(a, v)
 }
 
-func (v Value) AsByte() (byte, bool) {
-	return ValueTypes[v.Type].AsByte(v)
+func (v Value) AsByte(a *Arena) (byte, bool) {
+	return ValueTypes[v.Type].AsByte(a, v)
 }
 
-func (v Value) AsInt() (int64, bool) {
-	return ValueTypes[v.Type].AsInt(v)
+func (v Value) AsInt(a *Arena) (int64, bool) {
+	return ValueTypes[v.Type].AsInt(a, v)
 }
 
-func (v Value) AsFloat() (float64, bool) {
-	return ValueTypes[v.Type].AsFloat(v)
+func (v Value) AsFloat(a *Arena) (float64, bool) {
+	return ValueTypes[v.Type].AsFloat(a, v)
 }
 
-func (v Value) AsDecimal() (dec128.Dec128, bool) {
-	return ValueTypes[v.Type].AsDecimal(v)
+func (v Value) AsDecimal(a *Arena) (dec128.Dec128, bool) {
+	return ValueTypes[v.Type].AsDecimal(a, v)
 }
 
-func (v Value) AsTime() (time.Time, bool) {
-	return ValueTypes[v.Type].AsTime(v)
+func (v Value) AsTime(a *Arena) (time.Time, bool) {
+	return ValueTypes[v.Type].AsTime(a, v)
 }
 
-func (v Value) AsString() (string, bool) {
-	return ValueTypes[v.Type].AsString(v)
+func (v Value) AsString(a *Arena) (string, bool) {
+	return ValueTypes[v.Type].AsString(a, v)
 }
 
-func (v Value) AsRunes() ([]rune, bool) {
-	return ValueTypes[v.Type].AsRunes(v)
+func (v Value) AsRunes(a *Arena) ([]rune, bool) {
+	return ValueTypes[v.Type].AsRunes(a, v)
 }
 
-func (v Value) AsBytes() ([]byte, bool) {
-	return ValueTypes[v.Type].AsBytes(v)
+func (v Value) AsBytes(a *Arena) ([]byte, bool) {
+	return ValueTypes[v.Type].AsBytes(a, v)
 }
 
 func (v Value) AsArray(a *Arena) ([]Value, bool) {
-	return ValueTypes[v.Type].AsArray(v, a)
+	return ValueTypes[v.Type].AsArray(a, v)
 }
 
 func (v Value) AsDict(a *Arena) (map[string]Value, bool) {
-	return ValueTypes[v.Type].AsDict(v, a)
+	return ValueTypes[v.Type].AsDict(a, v)
 }
 
 func (v Value) UnaryOp(a *Arena, op token.Token) (Value, error) {
-	return ValueTypes[v.Type].UnaryOp(v, a, op)
+	return ValueTypes[v.Type].UnaryOp(a, v, op)
 }
 
 func (v Value) BinaryOp(a *Arena, op token.Token, rhs Value) (Value, error) {
-	return ValueTypes[v.Type].BinaryOp(v, a, op, rhs)
+	return ValueTypes[v.Type].BinaryOp(a, v, rhs, op)
 }
 
-func (v Value) Equal(rhs Value) bool {
-	return ValueTypes[v.Type].Equal(v, rhs)
+func (v Value) Equal(a *Arena, rhs Value) bool {
+	return ValueTypes[v.Type].Equal(a, v, rhs)
 }
 
-func (v *Value) Copy(a *Arena) (Value, error) {
-	return ValueTypes[v.Type].Copy(*v, a)
+func (v *Value) Clone(a *Arena) (Value, error) {
+	return ValueTypes[v.Type].Clone(a, *v)
 }
 
-func (v Value) MethodCall(vm VM, name string, args []Value) (Value, error) {
-	return ValueTypes[v.Type].MethodCall(v, vm, name, args)
+func (v Value) MethodCall(a *Arena, vm VM, name string, args []Value) (Value, error) {
+	return ValueTypes[v.Type].MethodCall(a, vm, v, name, args)
 }
 
-func (v Value) Access(vm VM, index Value, mode bc.Opcode) (Value, error) {
-	return ValueTypes[v.Type].Access(v, vm.Allocator(), index, mode)
+func (v Value) Access(a *Arena, index Value, mode bc.Opcode) (Value, error) {
+	return ValueTypes[v.Type].Access(a, v, index, mode)
 }
 
-func (v Value) Assign(idx Value, val Value) error {
-	return ValueTypes[v.Type].Assign(v, idx, val)
+func (v Value) Assign(a *Arena, idx Value, val Value) error {
+	return ValueTypes[v.Type].Assign(a, v, idx, val)
 }
 
 func (v Value) Iterator(a *Arena) (Value, error) {
-	return ValueTypes[v.Type].Iterator(v, a)
+	return ValueTypes[v.Type].Iterator(a, v)
 }
 
-func (v Value) Call(vm VM, args []Value) (Value, error) {
-	return ValueTypes[v.Type].Call(v, vm, args)
+func (v Value) Call(a *Arena, vm VM, args []Value) (Value, error) {
+	return ValueTypes[v.Type].Call(a, vm, v, args)
 }
 
-func (v Value) Len() int64 {
-	return ValueTypes[v.Type].Len(v)
+func (v Value) Len(a *Arena) int64 {
+	return ValueTypes[v.Type].Len(a, v)
 }
 
 func (v Value) Append(a *Arena, args []Value) (Value, error) {
-	return ValueTypes[v.Type].Append(v, a, args)
+	return ValueTypes[v.Type].Append(a, v, args)
 }
 
-func (v Value) Delete(key Value) (Value, error) {
-	return ValueTypes[v.Type].Delete(v, key)
+func (v Value) Delete(a *Arena, key Value) (Value, error) {
+	return ValueTypes[v.Type].Delete(a, v, key)
 }
 
 func (v Value) Slice(a *Arena, s Value, e Value) (Value, error) {
-	return ValueTypes[v.Type].Slice(v, a, s, e)
+	return ValueTypes[v.Type].Slice(a, v, s, e)
 }
 
 func (v Value) SliceStep(a *Arena, s Value, e Value, step Value) (Value, error) {
-	return ValueTypes[v.Type].SliceStep(v, a, s, e, step)
+	return ValueTypes[v.Type].SliceStep(a, v, s, e, step)
 }
 
 func (v Value) ToImmutable(a *Arena) (Value, error) {
