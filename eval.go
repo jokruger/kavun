ef9e676d09d9b52d2e1cccb298222a69e2836196
Yurii Biurher <yurii.biurher@zorallabs.com>
2026-06-11 13:42:03 +0300

    reinstate eval

---
+++ eval.go
@@ -0,0 +1,43 @@
+package kavun
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/jokruger/kavun/core"
+	"github.com/jokruger/kavun/vm"
+)
+
+// Eval compiles and executes given expr with params, and returns an evaluated value.
+// Argument `expr` must be an expression. Otherwise it will fail to compile.
+// Expression must not use or define variable "__res__" as it's reserved for the internal usage.
+func Eval(ctx context.Context, a *core.Arena, expr string, params map[string]any) (any, error) {
+	expr = strings.TrimSpace(expr)
+	if expr == "" {
+		return nil, fmt.Errorf("empty expression")
+	}
+
+	script := NewScript([]byte(fmt.Sprintf("__res__ := (%s)", expr)))
+	for pk := range params {
+		script.AddGlobals(pk)
+	}
+
+	compiled, err := script.Compile()
+	if err != nil {
+		return nil, fmt.Errorf("script compile: %w", err)
+	}
+	for k, v := range params {
+		nv, err := ValueOf(a, v)
+		if err != nil {
+			return nil, fmt.Errorf("convert param %q: %w", k, err)
+		}
+		compiled.Set(k, nv)
+	}
+
+	machine := vm.NewVM(vm.DefaultMaxFrames, vm.DefaultStackSize)
+	if err := compiled.RunContext(ctx, a, machine); err != nil {
+		return nil, fmt.Errorf("script run: %w", err)
+	}
+	return compiled.Get("__res__"), nil
+}
---
+++ value.go
@@ -0,0 +1,110 @@
+package kavun
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jokruger/dec128"
+	"github.com/jokruger/kavun/core"
+)
+
+func MustValueOf(a *core.Arena, v any) core.Value {
+	nv, err := ValueOf(a, v)
+	if err != nil {
+		panic(err)
+	}
+	return nv
+}
+
+func ValueOf(a *core.Arena, v any) (core.Value, error) {
+	switch v := v.(type) {
+	case nil:
+		return core.Undefined, nil
+
+	case bool:
+		return core.BoolValue(v), nil
+
+	case byte:
+		return core.ByteValue(v), nil
+
+	case []byte:
+		return a.NewBytesValue(v, false)
+
+	case rune:
+		return core.RuneValue(v), nil
+
+	case []rune:
+		return a.NewRunesValue(v, false)
+
+	case int:
+		return core.IntValue(int64(v)), nil
+
+	case int64:
+		return core.IntValue(v), nil
+
+	case float64:
+		return core.FloatValue(v), nil
+
+	case string:
+		return a.NewStringValue(v)
+
+	case dec128.Dec128:
+		return a.NewDecimalValue(v)
+
+	case time.Time:
+		return a.NewTimeValue(v)
+
+	case error:
+		nv, err := a.NewStringValue(v.Error())
+		if err != nil {
+			return core.Undefined, err
+		}
+		nv.Pin(a)
+		return a.NewErrorValue(nv, core.KindUser, false)
+
+	case []any:
+		arr := make([]core.Value, len(v))
+		for i, e := range v {
+			nv, err := ValueOf(a, e)
+			if err != nil {
+				return core.Undefined, err
+			}
+			nv.Pin(a)
+			arr[i] = nv
+		}
+		return a.NewArrayValue(arr, false)
+
+	case map[string]any:
+		kv := make(map[string]core.Value)
+		for vk, vv := range v {
+			nv, err := ValueOf(a, vv)
+			if err != nil {
+				return core.Undefined, err
+			}
+			nv.Pin(a)
+			kv[vk] = nv
+		}
+		return a.NewRecordValue(kv, false)
+
+	case core.NativeFunc:
+		return a.NewBuiltinClosureValue("anonymous", v, 0, true)
+
+	case core.Value:
+		return v, nil
+
+	case []core.Value:
+		for _, e := range v {
+			e.Pin(a)
+		}
+		return a.NewArrayValue(v, false)
+
+	case map[string]core.Value:
+		for _, vv := range v {
+			vv.Pin(a)
+		}
+		return a.NewRecordValue(v, false)
+
+	default:
+		return core.Undefined, fmt.Errorf("cannot convert to object: %T", v)
+	}
+}
