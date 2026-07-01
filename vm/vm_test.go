cffc4731a148c09bcd8aba12e26ab02ffc0617bd
Yurii Biurher <yurii.biurher@zorallabs.com>
2026-07-01 14:06:18 +0300

    more tests

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -3,8 +3,10 @@ package vm_test
 import (
 	"bytes"
 	"fmt"
+	"math"
 	"testing"
 
+	"github.com/jokruger/kavun/compiler"
 	"github.com/jokruger/kavun/core"
 	bc "github.com/jokruger/kavun/core/bytecode"
 	"github.com/jokruger/kavun/core/value"
@@ -644,4 +646,53 @@ func TestBytecodeConstFormatSpec(t *testing.T) {
 			mk("0,d"),
 		},
 	}))
+}
+
+func runVM(t *testing.T, instructions bc.Instructions, st core.Static) []core.Value {
+	t.Helper()
+	m := vm.NewVM(vm.DefaultMaxFrames, vm.DefaultStackSize)
+	globals := make([]core.Value, vm.GlobalsSize)
+	m.Reset(bytecode(instructions, st), globals)
+	require.NoError(t, m.Run())
+	return globals
+}
+
+func TestVM_PushOpcodes(t *testing.T) {
+	globals := runVM(t, concatInsts(
+		compiler.NewPushBool(true),
+		compiler.NewStoreGlobal(0),
+		compiler.NewPushBool(false),
+		compiler.NewStoreGlobal(1),
+		compiler.NewPushByte(255),
+		compiler.NewStoreGlobal(2),
+		compiler.NewPushRune('A'),
+		compiler.NewStoreGlobal(3),
+		compiler.NewPushInt(-1),
+		compiler.NewStoreGlobal(4),
+		compiler.NewPushInt(math.MinInt32),
+		compiler.NewStoreGlobal(5),
+		compiler.NewSuspend(),
+	), core.Static{})
+
+	require.Equal(t, core.BoolValue(true), globals[0])
+	require.Equal(t, core.BoolValue(false), globals[1])
+	require.Equal(t, core.ByteValue(255), globals[2])
+	require.Equal(t, core.RuneValue('A'), globals[3])
+	require.Equal(t, core.IntValue(-1), globals[4])
+	require.Equal(t, core.IntValue(math.MinInt32), globals[5])
+}
+
+func TestVM_JumpUsesInstructionIndex(t *testing.T) {
+	globals := runVM(t, concatInsts(
+		compiler.NewPushInt(1),
+		compiler.NewStoreGlobal(0),
+		compiler.NewJump(5),
+		compiler.NewPushInt(2),
+		compiler.NewStoreGlobal(0),
+		compiler.NewPushInt(3),
+		compiler.NewStoreGlobal(0),
+		compiler.NewSuspend(),
+	), core.Static{})
+
+	require.Equal(t, core.IntValue(3), globals[0])
 }
